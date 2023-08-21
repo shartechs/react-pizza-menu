51b8a7672f8832d9987d15a103cc94741b41c7bd
Shartechs <shartechs@gmail.com>
2023-08-22 00:13:27 +0200

    finilizing

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -8,12 +8,12 @@ export default function Pizza({
   soldOut,
 }) {
   return (
-    <div key={name} className="pizza">
+    <div key={name} className={`pizza ${soldOut ? "sold-out" : null}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+        <span>{soldOut ? "SOLD OUT" : price}</span>
       </div>
     </div>
   );
